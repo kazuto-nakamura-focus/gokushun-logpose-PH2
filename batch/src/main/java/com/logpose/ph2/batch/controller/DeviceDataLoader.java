d980fb9f68b7aa7ce868067f2e162045a33e4d19
takahashi.fumiaki <fumia@paul-ravaisson.com>
2023-10-11 16:01:24 +0900

    DBの向き先変更

--- batch/src/main/java/com/logpose/ph2/batch/controller/DeviceDataLoader.java
+++ batch/src/main/java/com/logpose/ph2/batch/controller/DeviceDataLoader.java
@@ -19,7 +19,6 @@ import com.logpose.ph2.batch.service.S2DeviceDayService;
 import com.logpose.ph2.batch.service.S3DailyBaseDataGeneratorService;
 import com.logpose.ph2.batch.service.S4ModelDataApplyrService;
 
-
 @Component
 public class DeviceDataLoader
 	{
@@ -27,14 +26,14 @@ public class DeviceDataLoader
 	// クラスメンバー
 	// ===============================================
 	private static Logger LOG = LogManager.getLogger(DeviceDataLoader.class);
-	
+
 	@Autowired
 	private ApplicationArguments arguments;
-	
+
 	private Long deviceId = null;
-	private boolean isAll = true;	
-	private String date = null;	
-	
+	private boolean isAll = true;
+	private String date = null;
+
 	@Autowired
 	S1DeviceDataLoaderService s1deviceDataLoaderService;
 	@Autowired
@@ -43,11 +42,11 @@ public class DeviceDataLoader
 	S3DailyBaseDataGeneratorService s3dailyBaseDataGeneratorService;
 	@Autowired
 	S4ModelDataApplyrService s4modelDataApplyrService;
-	
+
 	// ===============================================
 	// 公開関数群
 	// ===============================================
-		
+
 	// --------------------------------------------------------
 	/**
 	 * デバイスデータの生成を行う
@@ -56,53 +55,60 @@ public class DeviceDataLoader
 	 * @param date
 	 */
 	// --------------------------------------------------------
-	//public void load(Long deviceId, boolean isAll, String date) 
-	public void load() 
+	// public void load(Long deviceId, boolean isAll, String date)
+	public void load()
 		{
-		String args[] = arguments.getSourceArgs();
-		if( args[1].equals("-") )
+		try
 			{
-			deviceId = null;
-			}
-		else
-			{
-			deviceId = Long.valueOf(args[1]);
-			}
-		isAll = Boolean.valueOf(args[2]);
-		if( args[3].equals("-") )
-			{
-			date = null;
-			}
-		else
-			{
-			date =args[3];
-			}
-		
-		
-// * デバイスの指定が無い場合、全てのデバイスを対象とする
-		if(null == deviceId)
-			{
-			List<Ph2DevicesEnyity>  devices = this.s1deviceDataLoaderService.getDeviceAllInfo();
-			for(Ph2DevicesEnyity device : devices)
+			String args[] = arguments.getSourceArgs();
+			if (args[1].equals("-"))
 				{
-				this.loadDevice(device.getId(), this.isAll, this.date);
+				deviceId = null;
 				}
+			else
+				{
+				deviceId = Long.valueOf(args[1]);
+				}
+			isAll = Boolean.valueOf(args[2]);
+			if (args[3].equals("-"))
+				{
+				date = null;
+				}
+			else
+				{
+				date = args[3];
+				}
+
+// * デバイスの指定が無い場合、全てのデバイスを対象とする
+			if (null == deviceId)
+				{
+				List<Ph2DevicesEnyity> devices = this.s1deviceDataLoaderService.getDeviceAllInfo();
+				for (Ph2DevicesEnyity device : devices)
+					{
+					this.loadDevice(device.getId(), this.isAll, this.date);
+					}
+				}
+			else
+				{
+				this.loadDevice(this.deviceId, this.isAll, this.date);
+				}
+			System.exit(0);
 			}
-		else
+		catch (Exception e)
 			{
-			this.loadDevice(this.deviceId, this.isAll, this.date);
+			e.printStackTrace();
+			System.exit(-1);
 			}
-		System.exit(0);
 		}
-	
-	public void loadDevice(Long deviceId, boolean isAll, String date)
+
+	public void loadDevice(Long deviceId, boolean isAll, String date) throws RuntimeException
 		{
-		LOG.info("デバイスデータのローディングを開始します。", deviceId);
-//* 開始日の設定
+		LOG.info("デバイスデータのローディングを開始します。:" + deviceId);
+// * 開始日の設定
 		Date startDate = null;
-		if(!isAll)
+		if (!isAll)
 			{
-			if(date == null)
+			if (date == null)
 				{
 				// * 日付の指定が無い場合は、未処理の日付を取得する。
 				startDate = this.s1deviceDataLoaderService.getLatest(deviceId);
@@ -122,20 +128,21 @@ public class DeviceDataLoader
 			}
 		try
 			{
-			//* デバイス情報の取得
+			// * デバイス情報の取得
 			Ph2DevicesEnyity device = this.s1deviceDataLoaderService.getDeviceInfo(deviceId);
 			// * デバイス情報に開始日・タイムゾーン情報が無い場合終了
-			if((null == device.getBaseDate())||(null == device.getTz()))
+			if ((null == device.getBaseDate()) || (null == device.getTz()))
 				{
 				return;
 				}
-			
-			//* メッセージから基本情報の取得
+
+			// * メッセージから基本情報の取得
 			startDate = s1deviceDataLoaderService.loadMessages(device, startDate);
 			// * 日付をまたがった場合、以下の処理を行う
-			if(null != startDate )
+			if (null != startDate)
 				{
-				List<Ph2DeviceDayEntity> deviceDays = this.s2deviceDayService.initDeviceDay(device, startDate);
+				List<Ph2DeviceDayEntity> deviceDays = this.s2deviceDayService.initDeviceDay(device,
+						startDate);
 				this.s3dailyBaseDataGeneratorService.doService(deviceDays, startDate);
 				this.s4modelDataApplyrService.doService(deviceDays, startDate);
 				}
