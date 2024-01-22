469b3171aaeac87cebb562d814171932bcb49cfa
takahashi.fumiaki <fumia@paul-ravaisson.com>
2024-01-23 01:06:30 +0900

    リファクタリング2

--- backend/src/main/java/com/logpose/ph2/api/controller/dto/DataLoadDTO.java
+++ backend/src/main/java/com/logpose/ph2/api/controller/dto/DataLoadDTO.java
@@ -3,12 +3,18 @@ package com.logpose.ph2.api.controller.dto;
 import java.util.Date;
 
 import lombok.Data;
-
+/**
+ * コントローラ用のDTOクラス
+ * @since 2024/01/03
+ * @version 1.0
+ */
 @Data
 public class DataLoadDTO
 	{
-	//* デバイスID
+//* デバイスID
 	private Long deviceId;
+//* データ対象
 	private	Boolean isAll;
+//* データ取得開始日
 	private Date startDate;
 	}
--- backend/src/main/java/com/logpose/ph2/api/controller/dto/TargetParamDTO.java
+++ backend/src/main/java/com/logpose/ph2/api/controller/dto/TargetParamDTO.java
@@ -1,14 +1,18 @@
 package com.logpose.ph2.api.controller.dto;
 
 import lombok.Data;
-
+/**
+ * コントローラ用のDTOクラス
+ * @since 2024/01/03
+ * @version 1.0
+ */
 @Data
 public class TargetParamDTO
 	{
-	//* デバイスID
+//* デバイスID
 	private Long deviceId;
-	//* 年度
+//* 年度
 	private Short year;
-	// * パラメータセットID
+// * パラメータセットID
 	private Long paramId;
 	}
