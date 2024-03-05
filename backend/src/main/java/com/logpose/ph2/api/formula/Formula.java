dbf0eddc3e916075012e7fa9a81119278b35b347
takahashi.fumiaki <fumia@paul-ravaisson.com>
2024-03-05 13:57:55 +0900

    光合成推定計算の修正

--- backend/src/main/java/com/logpose/ph2/api/formula/Formula.java
+++ backend/src/main/java/com/logpose/ph2/api/formula/Formula.java
@@ -116,8 +116,9 @@ public class Formula
 		//＝Σ(((f*exp(g*PARdaily/600/日照時間*6)*PARdaily/600/日照時間*6/81.4/1000000)44*600*6)*日照時間*樹冠葉面積*1000)
 		// 日照時間
 		double sunHours = sunLight / 3600;
-		return prev +
-				((f * Math.exp(g * PAR/600/sunHours * 6)*PAR/600/sunHours*6/81.4/1000000)*44*600*6)*sunHours*(leafArea/1000);
+		
+		return prev + 
+				((f * Math.exp(g * PAR/600/60)*PAR/600/60/81.4/1000000)*44*600*6)*10*(leafArea/1000);
 		}
 
 	// --------------------------------------------------
