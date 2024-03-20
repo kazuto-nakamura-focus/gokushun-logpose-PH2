a3261f80ab0c94147b53570c5bade3d66d3c8df2
takahashi.fumiaki <fumia@paul-ravaisson.com>
2024-03-20 19:53:05 +0900

    リファクタリング

--- frontend/src/components-v1/parts/graph/Ph2ModelGraph.vue
+++ frontend/src/components-v1/parts/graph/Ph2ModelGraph.vue
@@ -1,47 +1,78 @@
 <template>
-  <v-card elevation-6>
-    <v-container>
-      <v-row>
-        <v-col>
-          <v-btn
-            class="ml-1"
-            v-for="(button, index) in editButtons"
-            depressed
-            color="primary"
-            elevation="3"
-            :key="index"
-            @click="openDialog(button)"
-          >{{ button.name }}</v-btn>
-        </v-col>
-        <v-col align="right">
-          <div>
-            <v-icon @click="handleClose()">mdi-close</v-icon>
-          </div>
-        </v-col>
-      </v-row>
-      <v-row style="height: 36px">
-        <v-col align="left" style="display: flex">
-          <div
-            v-if="annotationLabel"
-            style="
+  <v-app>
+    <v-card elevation-6>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-btn
+              class="ml-1"
+              v-for="(button, index) in editButtons"
+              depressed
+              color="primary"
+              elevation="3"
+              :key="index"
+              @click="openDialog(button)"
+            >{{ button.name }}</v-btn>
+          </v-col>
+          <v-col align="right">
+            <div>
+              <v-icon @click="handleClose()">mdi-close</v-icon>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row style="height: 36px">
+          <v-col align="left" style="display: flex">
+            <div
+              v-if="annotationLabel"
+              style="
             text-align: left;
               margin-left: 3px;
               padding: 5px;
               font-size: 10pt; font-family:Yu Gothic"
-          >
-            <b>{{ annotationLabel }}</b>
-          </div>
-        </v-col>
-      </v-row>
-      <v-row style="padding-bottom: 20px">
-        <v-col align="left" style="font-size: 10pt; font-family: Yu Gothic;margin-left:6px">
-          <b>{{ comment }}</b>
-        </v-col>
-      </v-row>
-    </v-container>
-    <!-- グラフ表示 -->
-    <ph-2-graphic-tool v-if="chartDisplay == true" :options="chart.options" :series="chart.series"></ph-2-graphic-tool>
-  </v-card>
+            >
+              <b>{{ annotationLabel }}</b>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row style="padding-bottom: 20px">
+          <v-col align="left" style="font-size: 10pt; font-family: Yu Gothic;margin-left:6px">
+            <b>{{ comment }}</b>
+          </v-col>
+        </v-row>
+      </v-container>
+      <!-- グラフ表示 -->
+      <ph-2-graphic-tool
+        v-if="chartDisplay == true"
+        :options="chart.options"
+        :series="chart.series"
+      ></ph-2-graphic-tool>
+    </v-card>
+    <div v-if="modelId=1">
+      <GEActualValueInput ref="refGEActualValueInput" :shared="sharedParam[0]" />
+      <ReferenceFValue ref="refReferenceFValue" :shared="sharedParam[1]" />
+      <parmeter-set-dialog ref="refGEParameterSets" :shared="sharedParam[2]" :modelId="modelId" />
+    </div>
+
+    <div v-if="modelId=2">
+      <LAActualValueInput
+        ref="refLAActualValueInput"
+        :shared="sharedParam[0]"
+        :selectedField="selectedMenu.selectedField"
+        :selectedDevices="selectedMenu.selectedDevices"
+      />
+      <parmeter-set-dialog ref="refLAParameterSets" :shared="sharedParam[1]" :modelId="modelId" />
+    </div>
+
+    <div v-if="modelId=3">
+      <PEActualValueInput
+        ref="refPEActualValueInput"
+        :shared="sharedParam[0]"
+        :selectedField="selectedMenu.selectedField"
+        :selectedDevices="selectedMenu.selectedDevices"
+      />
+      <parmeter-set-dialog ref="refPEParameterSets" :shared="sharedParam[1]" :modelId="photoModel" />
+    </div>
+  </v-app>
 </template>
     
 <script>
@@ -50,6 +81,11 @@ import "@mdi/font/css/materialdesignicons.css";
 import moment from "moment";
 import allEditButtons from "@/components-v1/parts/graph/editButtons.json";
 import { DialogController } from "@/lib/mountController.js";
+import GEActualValueInput from "@/components-v1/GrowthModel/RealInput/GEMainInput.vue";
+import ParmeterSetDialog from "@/components/TopStageGrowth/ParameterSet/ParmeterSetDialog.vue";
+import ReferenceFValue from "@/components-v1/GrowthModel/RealFValueIput/index.vue";
+import LAActualValueInput from "@/components-v1/LeafModel/RealValueInput/LAInput.vue";
+import PEActualValueInput from "@/components/TopStageGrowth/actualValueInput/PEInput.vue";
 
 export default {
   props: {
@@ -59,7 +95,12 @@ export default {
     },
   },
   components: {
-    ph2GraphicTool: Ph2GraphicTool,
+    Ph2GraphicTool,
+    GEActualValueInput,
+    ParmeterSetDialog,
+    ReferenceFValue,
+    LAActualValueInput,
+    PEActualValueInput,
   },
 
   data() {
@@ -137,6 +178,25 @@ export default {
       ];
 
       this.chartDisplay = true;
+    },
+    //* ============================================
+    // ダイアログのオープン
+    //* ============================================
+    openDialog: function (item) {
+      const selectedData = {
+        title: this.selectedMenu.selectedModel.name,
+        menu: this.selectedMenu,
+        dates: this.dates,
+      };
+      this.sharedParam[item.type].setUp(
+        this.$refs[item.key],
+        function (dailog) {
+          dailog.initialize(selectedData);
+        }.bind(this),
+        function (status) {
+          if (status) this.$refs.gfa.setGraphData(this.selectedMenu);
+        }.bind(this)
+      );
     },
     //* ============================================
     // 閉じる実行
