aaec8d6f82e26413e5717b15bc8b69c31722c787
takahashi.fumiaki <fumia@paul-ravaisson.com>
2024-10-04 11:37:46 +0900

    再デプロイ対応

--- backend/src/main/java/com/logpose/ph2/api/dto/sensorData/SensorDataDTO.java
+++ backend/src/main/java/com/logpose/ph2/api/dto/sensorData/SensorDataDTO.java
@@ -9,30 +9,30 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 public class SensorDataDTO extends GraphDataDTO
 	{
-	
+
 	private long interval = 0;
 	private List<String> category = new ArrayList<>();
 	private List<Double> nullData = new ArrayList<>();
-	
+
 	private int lastNullIndex = -1;
-	
+
 	public void setValue(Double value)
 		{
 		this.getValues().add(value);
 		this.nullData.add(null);
-		if( value != null )
+		if (value != null)
 			{
 // * 途中でNULLデータがある場合
-			if( lastNullIndex != -1)
+			if ((lastNullIndex != -1) && (lastNullIndex > 0))
 				{
-				double lastValue = this.getValues().get(lastNullIndex-1);
+				double lastValue = this.getValues().get(lastNullIndex - 1);
 				double diffIndex = this.getValues().size() - lastNullIndex;
-				double avediff = (value - lastValue) /diffIndex;
-				this.nullData.set(lastNullIndex-1, lastValue);
-				for(int i = lastNullIndex; i < this.getValues().size() ; i++)
+				double avediff = (value - lastValue) / diffIndex;
+				this.nullData.set(lastNullIndex - 1, lastValue);
+				for (int i = lastNullIndex; i < this.getValues().size(); i++)
 					{
 					lastValue += avediff;
 					this.nullData.set(i, lastValue);
@@ -42,9 +42,9 @@ public class SensorDataDTO extends GraphDataDTO
 			}
 		else
 			{
-			if( lastNullIndex == -1 )
+			if (lastNullIndex == -1)
 				{
-				lastNullIndex = this.getValues().size() -1;
+				lastNullIndex = this.getValues().size() - 1;
 				}
 			}
 		}
